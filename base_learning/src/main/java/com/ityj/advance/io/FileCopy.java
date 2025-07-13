6d538cd06214cbf9f628d06db031621d85fd1012
Jack <ayinjun1109@163.com>
2025-07-13 11:23:30 +0800

    test with modules

--- base_learning/src/main/java/com/ityj/advance/io/FileCopy.java
+++ base_learning/src/main/java/com/ityj/advance/io/FileCopy.java
@@ -7,8 +7,8 @@ public class FileCopy {
 
     public static void main(String[] args) {
 
-        File file = new File("src/main/resources/static/tt微信图片_20241230163708.jpg");
-        File file2 = new File("out.jpg");
+        File file = new File("base_learning/src/main/resources/static/tt微信图片_20241230163708.jpg");
+        File file2 = new File("base_learning/src/main/resources/static/out.jpg");
         try (FileInputStream fis = new FileInputStream(file);
              FileOutputStream fos = new FileOutputStream(file2)) {
             byte[] buff = new byte[8];
