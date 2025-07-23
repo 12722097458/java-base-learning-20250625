9b633cc635f0966b84511b96a54dc60efc9d9550
Jack <ayinjun1109@163.com>
2025-07-23 15:43:39 +0800

    init datasource

--- ssm/src/test/java/com/ityj/ssm/etity/StudentTest.java
+++ ssm/src/test/java/com/ityj/ssm/etity/StudentTest.java
@@ -3,15 +3,26 @@ package com.ityj.ssm.etity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+
 @SpringBootTest
 class StudentTest {
 
     @Autowired
     private Student student;
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
     public void testStudent() {
         System.out.println("student = " + student);
+    }
+
+    @Test
+    public void testDataSource() {
+        System.out.println("dataSource = " + dataSource);
     }
   
 }
