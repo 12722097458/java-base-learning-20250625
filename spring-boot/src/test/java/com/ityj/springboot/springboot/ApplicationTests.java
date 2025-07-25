9cbc0f0478d2c46e87a1a4bf6ad0fdfd8ca969cb
Jack <ayinjun1109@163.com>
2025-07-25 15:43:39 +0800

    slf4j + lomback

--- spring-boot/src/test/java/com/ityj/springboot/springboot/ApplicationTests.java
+++ spring-boot/src/test/java/com/ityj/springboot/springboot/ApplicationTests.java
@@ -1,13 +1,16 @@
 package com.ityj.springboot.springboot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.sql.DataSource;
 
+@Slf4j
 @SpringBootTest
 class ApplicationTests {
+
 
     @Autowired
     private DataSource dataSource;
@@ -18,6 +21,13 @@ class ApplicationTests {
         System.out.println(dataSource);
         // 添加配置  就切换为com.alibaba.druid.pool.DruidDataSource
         // 或添加一个配置bean  DruidDataSource    @Configuration
+    }
+
+    @Test
+    void testLog() {
+        log.debug("debug....");
+        log.info("info....");
+        log.warn("warn....");
     }
 
 }
