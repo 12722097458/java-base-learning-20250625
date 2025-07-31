9eefcb20056666e5dbdb1f78ed87bbe500e7fb3e
Jack <ayinjun1109@163.com>
2025-07-31 15:13:06 +0800

    common response format

--- spring-cloud-2024/cloud-provider-payment8001/src/main/java/com/ityj/cloud/controller/PaymentController.java
+++ spring-cloud-2024/cloud-provider-payment8001/src/main/java/com/ityj/cloud/controller/PaymentController.java
@@ -3,6 +3,7 @@ package com.ityj.cloud.controller;
 import com.ityj.cloud.entities.Pay;
 import com.ityj.cloud.entities.PayDTO;
 import com.ityj.cloud.service.PayService;
+import com.ityj.cloud.utils.ResultData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,7 +20,7 @@ public class PaymentController {
     private PayService payService;
 
     @GetMapping("/pay/getAll")
-    public List<PayDTO> queryAll() {
+    public ResultData<List<PayDTO>> queryAll() {
         List<Pay> data = payService.queryAll();
         List<PayDTO> result = new ArrayList<>();
         for (Pay datum : data) {
@@ -27,33 +28,33 @@ public class PaymentController {
             BeanUtils.copyProperties(datum, payDTO);
             result.add(payDTO);
         }
-        return result;
+        return ResultData.success(result);
     }
 
     @GetMapping("/pay/get/{id}")
-    public PayDTO queryById(@PathVariable("id") Integer id) {
+    public ResultData<PayDTO> queryById(@PathVariable("id") Integer id) {
         Pay pay = payService.queryById(id);
         PayDTO payDTO = new PayDTO();
         BeanUtils.copyProperties(pay, payDTO);
         log.info("payDTO is : {}", payDTO);
-        return payDTO;
+        return ResultData.success(payDTO);
     }
 
     @PostMapping(value = "/pay/add")
-    public String addPay(@RequestBody Pay pay){
+    public ResultData<String> addPay(@RequestBody Pay pay){
         int i = payService.insert(pay);
-        return "成功插入记录，返回值："+i;
+        return ResultData.success("成功插入记录，返回值："+i);
     }
     @DeleteMapping(value = "/pay/del/{id}")
-    public Integer deletePay(@PathVariable("id") Integer id) {
-        return payService.deleteById(id);
+    public ResultData<Integer> deletePay(@PathVariable("id") Integer id) {
+        return ResultData.success(payService.deleteById(id));
     }
     @PutMapping(value = "/pay/update")
-    public String updatePay(@RequestBody PayDTO payDTO){
+    public ResultData<String> updatePay(@RequestBody PayDTO payDTO){
         Pay pay = new Pay();
         BeanUtils.copyProperties(payDTO, pay);
         int i = payService.updateById(pay);
-        return "成功修改记录，返回值："+i;
+        return ResultData.success("成功修改记录，返回值：" + i);
     }
 
 
