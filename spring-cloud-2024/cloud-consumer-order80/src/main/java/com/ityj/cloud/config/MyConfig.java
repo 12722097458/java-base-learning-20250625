a46196ae7944b77ce7c8229a1066cb9490955944
Jack <ayinjun1109@163.com>
2025-08-01 12:38:50 +0800

    修改为随机的负载均衡策略

--- spring-cloud-2024/cloud-consumer-order80/src/main/java/com/ityj/cloud/config/MyConfig.java
+++ spring-cloud-2024/cloud-consumer-order80/src/main/java/com/ityj/cloud/config/MyConfig.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@Configuration
+//@Configuration
 public class MyConfig {
 
     @Bean
-    @LoadBalanced // 开启默认的负载均衡  - 本地端    nginx是服务器端
+    @LoadBalanced // 默认轮询RoundRobinLoadBalancer 开启默认的负载均衡  - 本地端    nginx是服务器端
     public RestTemplate getRestTemplate() {
         return new RestTemplate();
     }
