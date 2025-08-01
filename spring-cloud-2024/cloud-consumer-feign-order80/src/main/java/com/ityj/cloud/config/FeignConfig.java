a52e16a35ac5d44c7f4f8c9ff12654b87da1289c
Jack <ayinjun1109@163.com>
2025-08-01 13:51:27 +0800

    open feign 开启服务调用日志打印功能，可以测试出retry和请求响应压缩

--- spring-cloud-2024/cloud-consumer-feign-order80/src/main/java/com/ityj/cloud/config/FeignConfig.java
+++ spring-cloud-2024/cloud-consumer-feign-order80/src/main/java/com/ityj/cloud/config/FeignConfig.java
@@ -1,5 +1,6 @@
 package com.ityj.cloud.config;
 
+import feign.Logger;
 import feign.Retryer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,10 +10,15 @@ public class FeignConfig {
 
     @Bean
     public Retryer getRetryer() {
-        return Retryer.NEVER_RETRY; //Feign默认配置是不走重试策略的
+//        return Retryer.NEVER_RETRY; //Feign默认配置是不走重试策略的
 
         //最大请求次数为3(1+2)，初始间隔时间为100ms，重试间最大间隔时间为1s
-        // return new Retryer.Default(100,1,3);
+        return new Retryer.Default(100,1,3);
+    }
+
+    @Bean
+    public Logger.Level level() {
+        return Logger.Level.FULL;
     }
 
 }
