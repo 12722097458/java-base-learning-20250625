15564f9a531f4b7c8c7fd25fe8160837334acb76
Jack <ayinjun1109@163.com>
2025-08-01 12:32:13 +0800

    DiscoveryClient 可以拿到所有Consul注册的服务，可以实现负载均衡

--- spring-cloud-2024/cloud-consumer-order80/src/main/java/com/ityj/cloud/controller/OrderController.java
+++ spring-cloud-2024/cloud-consumer-order80/src/main/java/com/ityj/cloud/controller/OrderController.java
@@ -5,10 +5,14 @@ import com.ityj.cloud.response.ResultData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -34,6 +38,25 @@ public class OrderController {
     @GetMapping(value = "/consumer/pay/info")
     public ResultData<String> info(){
         return restTemplate.getForObject(PAYMENT_BASE_URL + "/pay/info", ResultData.class);
+    }
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+    @GetMapping("/consumer/discovery")
+    public String discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            System.out.println(element);
+        }
+
+        System.out.println("===================================");
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("cloud-payment-service");
+        for (ServiceInstance element : instances) {
+            System.out.println(element.getServiceId()+"\t"+element.getHost()+"\t"+element.getPort()+"\t"+element.getUri());
+        }
+
+        return instances.get(0).getServiceId()+":"+instances.get(0).getPort();
     }
 
 }
