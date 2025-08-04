4dae3d52e001c5a0c035ed39f792e79271e95eff
Jack <ayinjun1109@163.com>
2025-08-04 13:59:55 +0800

    Spring Cloud Alibaba- Sentinel 流控规则-流控模式 1. 直接 -> 默认模式 就是针对自己 2. 关联 -> /B突然流量达到阈值，/A进行限流 3. 链路 -> 访问相同资源的api, 只针对配置了入口的进行限流

--- spring-cloud-2024/cloudalibaba-sentinel-service8401/src/main/java/com/ityj/cloud/controller/FlowLimitController.java
+++ spring-cloud-2024/cloudalibaba-sentinel-service8401/src/main/java/com/ityj/cloud/controller/FlowLimitController.java
@@ -1,5 +1,7 @@
 package com.ityj.cloud.controller;
 
+import com.ityj.cloud.service.FlowLimitService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +16,25 @@ public class FlowLimitController {
     @GetMapping("/testB")
     public String testB() {
         return "------testB";
+    }
+
+    /**
+     * 流控-链路演示demo
+     * C和D两个请求都访问flowLimitService.common()方法，阈值到达后对C限流，对D不管
+     */
+    @Autowired
+    private FlowLimitService flowLimitService;
+
+    @GetMapping("/testC")
+    public String testC() {
+        flowLimitService.common();
+        return "------testC";
+    }
+
+    @GetMapping("/testD")
+    public String testD() {
+        flowLimitService.common();
+        return "------testD";
     }
 }
  
