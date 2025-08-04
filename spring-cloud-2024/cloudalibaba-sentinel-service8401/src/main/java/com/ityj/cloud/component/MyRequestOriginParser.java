50505c530c0bf61f2ec4b1da7d7b6cdb2b403f96
Jack <ayinjun1109@163.com>
2025-08-04 16:01:38 +0800

    Spring Cloud Alibaba- Sentinel 授权 1. 通过配置类RequestOriginParser处理 2. 如果配置了黑名单 那么就不允许配置用户访问 3. 配置的是白名单，仅允许配置用户访问

---
+++ spring-cloud-2024/cloudalibaba-sentinel-service8401/src/main/java/com/ityj/cloud/component/MyRequestOriginParser.java
@@ -0,0 +1,18 @@
+package com.ityj.cloud.component;
+
+import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.RequestOriginParser;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyRequestOriginParser implements RequestOriginParser {
+    @Override
+    public String parseOrigin(HttpServletRequest httpServletRequest) {
+        return httpServletRequest.getParameter("serverName");
+    }
+}
+ 
+ 
+
+ 
+
