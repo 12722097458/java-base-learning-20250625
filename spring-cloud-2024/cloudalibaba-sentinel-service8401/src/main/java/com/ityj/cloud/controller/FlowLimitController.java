bbbd609e7a778b64adc4a12a4cd5e597305872df
Jack <ayinjun1109@163.com>
2025-08-04 14:39:08 +0800

    Spring Cloud Alibaba- Sentinel 流控规则-熔断规则 1. 慢调用比例 - 统计时长1秒内最小请求数5满足的情况下，调用请求的response time 大于配置值的比例大于比例阈值就会熔断。 时间到熔断时间5秒后的下一个请求如果不报错，变为close状态，系统恢复正常，否则还是open状态。 2. 异常比例： 报错的比例 3. 异常数： 报错数量

--- spring-cloud-2024/cloudalibaba-sentinel-service8401/src/main/java/com/ityj/cloud/controller/FlowLimitController.java
+++ spring-cloud-2024/cloudalibaba-sentinel-service8401/src/main/java/com/ityj/cloud/controller/FlowLimitController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
 public class FlowLimitController {
 
@@ -41,6 +43,35 @@ public class FlowLimitController {
         System.out.println(System.currentTimeMillis() + "      testE,排队等待");
         return "------testE";
     }
+
+    /**
+     * 新增熔断规则-慢调用比例
+     *
+     * @return
+     */
+    @GetMapping("/testF")
+    public String testF() {
+        //暂停几秒钟线程
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("----测试:新增熔断规则-慢调用比例 ");
+        return "------testF 新增熔断规则-慢调用比例";
+    }
+
+    /**
+     * 新增熔断规则-异常比例
+     *
+     * @return
+     */
+    @GetMapping("/testG")
+    public String testG() {
+        int a = 1 / 0;
+        return "------testF 新增熔断规则-异常比例";
+    }
+
 
 }
  
