836eb6e5e8a687fb51a890469b842e3d69ddc61c
Jack <ayinjun1109@163.com>
2025-08-04 15:14:18 +0800

    Spring Cloud Alibaba- Sentinel @SentinelResource
    1. blockHandler 是满足流控规则是调用的方法
    2. fallback  是系统报错时调用的降级方法

---
+++ spring-cloud-2024/cloudalibaba-sentinel-service8401/src/main/java/com/ityj/cloud/controller/RateLimitController.java
@@ -0,0 +1,38 @@
+package com.ityj.cloud.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class RateLimitController {
+    @GetMapping("/rateLimit/byUrl")
+    public String byUrl() {
+        return "按rest地址限流测试OK";
+    }
+
+    @GetMapping("/rateLimit/byResourceSentinel")
+    @SentinelResource(value = "byResourceSentinelResource",
+            blockHandler = "blockHandlerMethod",
+            fallback = "fallbackMethod")
+    public String byResource(@RequestParam(value = "p1", required = false) Integer p1) {
+        if (p1 != null) {
+            int a = 3 / p1;
+        }
+        return "按rest地址限流测试OK - " + p1;
+    }
+
+    public String blockHandlerMethod(@RequestParam(value = "p1", required = false) Integer p1, BlockException e) {
+        return "blockHandlerMethod()..." + e.getMessage();
+    }
+
+    public String fallbackMethod(@RequestParam(value = "p1", required = false) Integer p1, Throwable t) {
+        return "fallbackMethod()... " + t.getMessage();
+    }
+}
+
+ 
