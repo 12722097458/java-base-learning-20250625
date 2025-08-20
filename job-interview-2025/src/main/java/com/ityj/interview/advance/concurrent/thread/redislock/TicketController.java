dafd4f92af53fd9893dca447eda3333b5b9727ea
Jack <ayinjun1109@163.com>
2025-08-20 11:38:04 +0800

    redis lock: redis集群可以进入RedLock来处理----在 (N/2 + 1) 个独立的 Redis 主节点上获取锁，才算成功，用以容忍少数节点故障

--- job-interview-2025/src/main/java/com/ityj/interview/advance/concurrent/thread/redislock/TicketController.java
+++ job-interview-2025/src/main/java/com/ityj/interview/advance/concurrent/thread/redislock/TicketController.java
@@ -32,6 +32,7 @@ public class TicketController {
     public synchronized String sell(@PathVariable("productId") int productId) throws InterruptedException {
 
         RLock lock = redisson.getLock(REDIS_TEST_LOCK);
+        //        RLock redLock = redisson.getRedLock(lock);  在 (N/2 + 1) 个独立的 Redis 主节点上获取锁，才算成功，用以容忍少数节点故障
         lock.lock();
         TimeUnit.MINUTES.sleep(3);
         try {
