bdf806af7f3eb829f88e7491c1434129b030148c
Jack <ayinjun1109@163.com>
2025-08-20 11:37:19 +0800

    redis lock: 引入Redisson.getLock来解决分布式锁问题。 看门狗机制 默认key的过期时间为30秒，10秒后自动续期(ttl  20 - 30)

--- job-interview-2025/src/main/java/com/ityj/interview/advance/concurrent/thread/redislock/TicketController.java
+++ job-interview-2025/src/main/java/com/ityj/interview/advance/concurrent/thread/redislock/TicketController.java
@@ -2,10 +2,16 @@ package com.ityj.interview.advance.concurrent.thread.redislock;
 
 import com.ityj.interview.advance.concurrent.thread.redislock.service.ProductService;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.Serializable;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @RestController
@@ -14,19 +20,35 @@ public class TicketController {
     @Autowired
     private ProductService productService;
 
+    private static final String REDIS_TEST_LOCK = "REDIS_TEST_LOCK";
+
+    @Autowired
+    private RedisTemplate<String, Serializable> redisTemplate;
+
+    @Autowired
+    private Redisson redisson;
+
     @RequestMapping("/sell/{productId}")
-    public synchronized String sell(@PathVariable("productId") int productId) {
-        int balance = productService.getBalance(productId);
-        if (balance > 0) {
-            int result = productService.updateBalance(productId, balance - 1);
-            if (result > 0) {
-                log.info("{} 抢到{}", Thread.currentThread().getName(), balance);
-                return "success";
-            } else {
-                log.error("error {} 抢到{}，但数据库更新失败！", Thread.currentThread().getName(), balance);
+    public synchronized String sell(@PathVariable("productId") int productId) throws InterruptedException {
+
+        RLock lock = redisson.getLock(REDIS_TEST_LOCK);
+        lock.lock();
+        TimeUnit.MINUTES.sleep(3);
+        try {
+            int balance = productService.getBalance(productId);
+            if (balance > 0) {
+                int result = productService.updateBalance(productId, balance - 1);
+                if (result > 0) {
+                    log.info("{} 抢到{}", Thread.currentThread().getName(), balance);
+                    return "success";
+                } else {
+                    log.error("error {} 抢到{}，但数据库更新失败！", Thread.currentThread().getName(), balance);
+                }
             }
+            return "fail!";
+        } finally {
+            lock.unlock();
         }
-        return "fail!";
     }
 }
 
---
+++ job-interview-2025/src/main/java/com/ityj/interview/advance/concurrent/thread/redislock/config/RedisConfig.java
@@ -0,0 +1,42 @@
+package com.ityj.interview.advance.concurrent.thread.redislock.config;
+
+import org.redisson.Redisson;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
+
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
+
+    /**
+     * 保证不是序列化后的乱码配置
+     */
+    @Bean
+    public RedisTemplate<String, Serializable> redisTemplate(LettuceConnectionFactory connectionFactory){
+        RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+
+    @Bean
+    public Redisson redisson(){
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://" + redisHost + ":6379").setDatabase(0);
+        return (Redisson) Redisson.create(config);
+    }
+}
+ 
+ 
