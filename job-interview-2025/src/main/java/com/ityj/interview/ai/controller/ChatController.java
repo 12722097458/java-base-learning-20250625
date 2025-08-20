91927e3ef88732746fc5167606fc8362517a4195
Jack <ayinjun1109@163.com>
2025-08-20 16:54:57 +0800

    spring ai alibaba test
    https://opendeep.wiki/alibaba/spring-ai-alibaba/quickstart
    
    https://bailian.console.aliyun.com/?spm=a2c4g.11186623.0.0.8f297980PEQnTd&apiKey=1#/api-key

---
+++ job-interview-2025/src/main/java/com/ityj/interview/ai/controller/ChatController.java
@@ -0,0 +1,24 @@
+package com.ityj.interview.ai.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+     private final ChatClient chatClient;
+
+     public ChatController(ChatClient.Builder builder) {
+         this.chatClient = builder.build();
+     }
+
+     @GetMapping("/chat")
+     public String chat(@RequestParam("input") String input) {
+      return this.chatClient.prompt()
+        .user(input)
+        .call()
+        .content();
+     }
+}
