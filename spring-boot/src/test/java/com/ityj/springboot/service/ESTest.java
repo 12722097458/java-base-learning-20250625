9e450b47df3b915fc49bdfde9323fc95e3df79ba
Jack <ayinjun1109@163.com>
2025-09-12 15:57:34 +0800

    Elasticsearch索引库操作

--- spring-boot/src/test/java/com/ityj/springboot/service/ESTest.java
+++ spring-boot/src/test/java/com/ityj/springboot/service/ESTest.java
@@ -1,9 +1,11 @@
 package com.ityj.springboot.service;
 
 import lombok.extern.slf4j.Slf4j;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.xcontent.XContentType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,6 +31,25 @@ public class ESTest {
         // 3.发送请求
         restHighLevelClient.indices().create(request, RequestOptions.DEFAULT);
     }
+
+    @Test
+    void testDeleteHotelIndex() throws IOException {
+        // 1.创建Request对象
+        DeleteIndexRequest request = new DeleteIndexRequest("hotel");
+        // 2.发送请求
+        restHighLevelClient.indices().delete(request, RequestOptions.DEFAULT);
+    }
+
+    @Test
+    void testExistsHotelIndex() throws IOException {
+        // 1.创建Request对象
+        GetIndexRequest request = new GetIndexRequest("hotel");
+        // 2.发送请求
+        boolean exists = restHighLevelClient.indices().exists(request, RequestOptions.DEFAULT);
+        // 3.输出
+        System.err.println(exists ? "索引库已经存在！" : "索引库不存在！");
+    }
+
 
 }
 
