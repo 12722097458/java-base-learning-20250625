3b4a100571622d3fdc3f821b1fb2076ff541c7ca
Jack <ayinjun1109@163.com>
2025-10-10 14:23:57 +0800

    saa-01helloworld - add application name

--- spring-ai-alibaba-parent/saa-01helloworld/src/main/java/com/ityj/ai/config/SaaConfig.java
+++ spring-ai-alibaba-parent/saa-01helloworld/src/main/java/com/ityj/ai/config/SaaConfig.java
@@ -5,8 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
-
 //@Configuration
 // 无需配置类，项目启动后会自动配置，自动加载modelName，apiKey和baseUrl
 // 其中modelName和baseUrl都有默认值
