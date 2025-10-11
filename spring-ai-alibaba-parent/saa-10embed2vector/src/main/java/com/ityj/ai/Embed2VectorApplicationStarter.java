9418d2bc134e1daea1a1bb3c6a41a6f57ff27b41
Jack <ayinjun1109@163.com>
2025-10-11 16:23:24 +0800

    saa-10embed2vector - 向量化及像两户数据库存取 init

---
+++ spring-ai-alibaba-parent/saa-10embed2vector/src/main/java/com/ityj/ai/Embed2VectorApplicationStarter.java
@@ -0,0 +1,12 @@
+package com.ityj.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 向量化和向量数据库
+@SpringBootApplication
+public class Embed2VectorApplicationStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(Embed2VectorApplicationStarter.class);
+    }
+}
