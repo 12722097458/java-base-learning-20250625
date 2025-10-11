d5f12e6989357b2e861151f5b9ba007af2db1cc1
Jack <ayinjun1109@163.com>
2025-10-11 14:31:51 +0800

    saa-07chat-persistent - 聊天记录持久化

---
+++ spring-ai-alibaba-parent/saa-07chat-persistent/src/main/java/com/ityj/ai/controller/ChatPersistentController.java
@@ -0,0 +1,25 @@
+package com.ityj.ai.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@Slf4j
+@RestController
+public class ChatPersistentController {
+
+    @Autowired
+    private ChatClient qwenChatClient;
+
+    @GetMapping("/chat/persistent")
+    public Flux<String> chatStream(@RequestParam(value = "content", defaultValue = "你是谁？") String content) {
+        return qwenChatClient.prompt().user(content).stream().content();
+    }
+
+
+
+}
